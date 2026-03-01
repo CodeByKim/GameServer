bb2f012defa0eafc3fee5d348c51e7219a8a9dce
garamkim <codebykim@gmail.com>
2026-03-02 02:11:41 +0900

    Config 파일 로드

---
+++ Core/Config/AppConfig.cs
@@ -0,0 +1,11 @@
+﻿namespace Core.Config;
+
+internal class AppConfig
+{
+    public ServerConfig Server { get; init; } = null!;
+}
+
+internal class ServerConfig
+{
+    public int Port { get; init; }
+}
--- Core/Server/ServerLauncher.cs
+++ Core/Server/ServerLauncher.cs
@@ -1,5 +1,7 @@
 ﻿namespace Core.Server;
 
+using Core.Config;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 
@@ -10,9 +12,16 @@ public static class ServerLauncher
         var host = Host.CreateDefaultBuilder()
             .ConfigureAppConfiguration((context, config) =>
             {
+                config.SetBasePath(AppContext.BaseDirectory).AddJsonFile($"Config.json");
             })
             .ConfigureServices((context, services) =>
             {
+                var config = context.Configuration.Get<AppConfig>();
+                if (config == null)
+                {
+                    throw new ArgumentNullException(nameof(config));
+                }
+
                 services.AddHostedService(provider => new NetworkService(server));
             })
             .Build();
